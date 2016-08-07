a79bd3c345bca81427303849a4af129d5a13c412
BitLooter <pwnzerdragoon+gh@gmail.com>
2016-08-07 05:39:15 -0700

    Created generic thunk creator

--- app/actions.js
+++ app/actions.js
@@ -1,57 +1,56 @@
 // actions.js -- Contains actions used by the code.
+//
+// Async request actions should follow this pattern:
+// VERB_NOUN__(REQUEST|DONE|ERROR)
+// REQUEST is dispatched when the request is made, DONE when successfully
+// retrieved, and ERROR if any sort of error occured.
 
 import { createAction } from 'redux-actions'
 
-// export const REQUEST_THING_LIST = 'REQUEST_THING_LIST'
-// export const RECEIVE_THING_LIST = 'RECEIVE_THING_LIST'
-// export const RECEIVE_THING_LIST_ERROR = 'RECEIVE_THING_LIST_ERROR'
-// export const REQUEST_ADD_THING = 'REQUEST_ADD_THING'
-// export const RECEIVE_ADD_THING = 'RECEIVE_ADD_THING'
-// export const RECEIVE_ADD_THING_ERROR = 'RECEIVE_ADD_THING_ERROR'
+// Generic thunk creator for backend API requests. *Action parameters should
+// be the appropriate action creators, and apiFunction is an async function
+// that makes the request.
+function createApiThunk (apiFunction, requestAction, doneAction, errorAction) {
+  return function (apiParams) {
+    return async function (dispatch) {
+      dispatch(requestAction())
+      try {
+        const response = await apiFunction(apiParams)
+        dispatch(doneAction(response))
+      } catch (error) {
+        dispatch(errorAction(error))
+      }
+    }
+  }
+}
+
 export const SHOW_THING_INFO = 'SHOW_THING_INFO'
 
 export const showThingInfo = createAction(SHOW_THING_INFO)
 
-// Async request actions. They should follow this pattern:
-// VERB_NOUN__(REQUEST|DONE|ERROR)
-// REQUEST is dispatched when the request is made, DONE when successfully
-// retrieved, and ERROR if any sort of error occured.
+// Actions to GET things from the server.
 export const GET_THING_LIST__REQUEST = 'GET_THING_LIST__REQUEST'
 export const GET_THING_LIST__DONE = 'GET_THING_LIST__DONE'
 export const GET_THING_LIST__ERROR = 'GET_THING_LIST__ERROR'
-export const POST_THING__REQUEST = 'POST_THING__REQUEST'
-export const POST_THING__DONE = 'POST_THING__DONE'
-export const POST_THING__ERROR = 'POST_THING__ERROR'
-
 export const getThingListRequest = createAction(GET_THING_LIST__REQUEST)
 export const getThingListDone = createAction(GET_THING_LIST__DONE)
 export const getThingListError = createAction(GET_THING_LIST__ERROR)
+export const getThingList = createApiThunk(
+  async () => { return global.stuffrapi.getThings() },
+  getThingListRequest, getThingListDone, getThingListError
+)
 
-/* Thunk action to get a list of things from the server. */
-export function getThingList () {
-  return async function (dispatch) {
-    dispatch(getThingListRequest())
-    try {
-      const thingList = await global.stuffrapi.getThings()
-      dispatch(getThingListDone(thingList))
-    } catch (error) {
-      dispatch(getThingListError(error))
-    }
-  }
-}
-
+// Actions to POST a new thing to the server.
+export const POST_THING__REQUEST = 'POST_THING__REQUEST'
+export const POST_THING__DONE = 'POST_THING__DONE'
+export const POST_THING__ERROR = 'POST_THING__ERROR'
 export const postThingRequest = createAction(POST_THING__REQUEST)
 export const postThingDone = createAction(POST_THING__DONE)
 export const postThingError = createAction(POST_THING__ERROR)
-
-export function postThing (thing) {
-  return async function (dispatch) {
-    dispatch(postThingRequest())
-    try {
-      const thingResponse = await global.stuffrapi.addThing(thing)
-      dispatch(postThingDone({...thing, id: thingResponse.id}))
-    } catch (error) {
-      dispatch(postThingError(error))
-    }
-  }
-}
+export const postThing = createApiThunk(
+  async function (thing) {
+    const thingResponse = await global.stuffrapi.addThing(thing)
+    return {...thing, id: thingResponse.id}
+  },
+  postThingRequest, postThingDone, postThingError
+)
