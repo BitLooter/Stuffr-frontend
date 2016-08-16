4bf525fd1745894452d190a2db092f8db2246519
BitLooter <pwnzerdragoon+gh@gmail.com>
2016-08-15 23:13:43 -0700

    Request function autodetects method

--- app/stuffrapi.js
+++ app/stuffrapi.js
@@ -2,8 +2,9 @@ import log from 'loglevel'
 
 export class StuffrApi {
   constructor (baseUrl) {
-    if (!baseUrl.endsWith('/')) {
-      baseUrl = `${baseUrl}/`
+    if (baseUrl.endsWith('/')) {
+      // Strip trailing slash
+      baseUrl = baseUrl.replace(/\/$/, '')
     }
     this.urlBase = baseUrl
   }
@@ -11,23 +12,30 @@ export class StuffrApi {
   // GET request to /things
   async getThings (callback) {
     log.info('StuffrApi request for getThings')
-    return await this._request('things', {callback})
+    return await this._request('/things', {callback})
   }
 
   // POST request to /things
   async addThing (parameters, callback) {
     log.info('StuffrApi request for addThing')
-    return await this._request('things', {method: 'POST', parameters, callback})
+    return await this._request('/things', {parameters, callback})
   }
 
   // Makes request to the server specified in baseUrl
-  async _request (path, {method = 'GET', parameters, callback} = {}) {
+  async _request (path, {method, parameters, callback} = {}) {
     // TODO: Autodetect method based on presence of parameters
     const headers = new Headers()
     const body = JSON.stringify(parameters)
     const fullUrl = this.urlBase + path
     log.trace(`StuffrApi generic request to ${fullUrl}`)
     // Default method is GET, POST if paramater data is given
+    if (!method) {
+      if (parameters) {
+        method = 'POST'
+      } else {
+        method = 'GET'
+      }
+    }
     if (method === 'POST' || method === 'PUT') {
       headers.append('Content-Type', 'application/json')
     }
@@ -41,7 +49,7 @@ export class StuffrApi {
       throw new Error(`Unable to fetch ${fullUrl}`)
     }
     if (!response.ok) {
-      throw new Error(`Error: HTTP response ${response.status} '${response.statusText}' fetching ${fullUrl}`)
+      throw new Error(`HTTP response ${response.status} '${response.statusText}' fetching ${fullUrl}`)
     }
 
     const returnValue = await response.json()
