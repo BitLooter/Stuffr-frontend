d6426db4f8582db7f89fe8ab5d6fce56e487ab0f
BitLooter <pwnzerdragoon+gh@gmail.com>
2017-09-02 18:12:26 -0700

    Refactored all forms to use Formik

--- app/components/InventoryEditDialog.js
+++ app/components/InventoryEditDialog.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { Formik } from 'formik'
+import yup from 'yup'
 import log from 'loglevel'
+import Dialog from 'material-ui/Dialog'
 import FlatButton from 'material-ui/FlatButton'
 import RaisedButton from 'material-ui/RaisedButton'
 import TextField from 'material-ui/TextField'
@@ -8,86 +11,68 @@ import moment from 'moment'
 
 import { submitInventory, closeInventoryEditor } from '../actions'
 import t from '../i18n'
-import { isString, isEmpty } from '../util'
-import FormDialogBase from './FormDialogBase'
 
 const DIALOG_NEW = Symbol.for('ui.DIALOG_NEW')
 const DIALOG_EDIT = Symbol.for('ui.DIALOG_EDIT')
 
-@connect(undefined, {submitInventory, closeInventoryEditor})
-export default class InventoryEditDialog extends FormDialogBase {
-  static fields = ['name']
-
-  constructor (props) {
-    super(props, props.inventory)
-    this.buttons = <div>
-      <FlatButton
-        label={t('common.cancel')}
-        onClick={this.handleCancel}
-      />
-      <RaisedButton
-        primary={true}
-        label={t('common.save')}
-        onClick={this.handleDone}
-      />
-    </div>
-  }
-
-  validateForm = () => {
-    const data = this.state.data
-    const errors = {}
-
-    if (isString(data.name)) {
-      if (data.name.length === 0) {
-        errors.name = t('inventory.nameErrorMissing')
-      }
+const formikWrapper = Formik({
+  mapPropsToValues: (props) => ({
+    name: ''
+  }),
+  validationSchema: yup.object().shape({
+    name: yup.string().required(t('inventory.nameErrorMissing'))
+  }),
+  handleSubmit: (values, {props}) => {
+    if (props.mode === DIALOG_EDIT) {
+      // TODO: only submit changed fields
+      log.info(`Updating existing inventory with id ${props.inventory.id}`)
+      props.submitInventory(values, props.inventory.id)
+    } else if (props.mode === DIALOG_NEW) {
+      log.info(`Creating new inventory named ${values.name}`)
+      props.submitInventory(values)
     } else {
-      log.error(`Name should be a string, found ${typeof data.name} instead`)
+      const errorMessage = `Unknown mode for InventoryEditDialog: ${String(props.mode)}`
+      log.error(errorMessage)
+      throw new Error(errorMessage)
     }
-
-    this.setState({errors})
-    return isEmpty(errors)
+    // TODO: some sort of loading animation while waiting on the HTTP request
   }
+})
+const reduxWrapper = connect(undefined, {submitInventory, closeInventoryEditor})
+export default reduxWrapper(formikWrapper(({
+  inventory, mode,
+  closeInventoryEditor,
+  errors,
+  handleChange, handleSubmit
+}) => {
+  const buttons = <div>
+    <FlatButton
+      label={t('common.cancel')}
+      onClick={closeInventoryEditor}
+    />
+    <RaisedButton
+      primary={true}
+      label={t('common.save')}
+      onClick={handleSubmit}
+    />
+  </div>
+  const title = mode === DIALOG_EDIT
+    ? inventory.name : t('inventory.newTitle')
 
-  handleDone = () => {
-    if (this.validateForm()) {
-      if (this.props.mode === DIALOG_EDIT) {
-        const changedData = this.getChangedData()
-        if (!isEmpty(changedData)) {
-          log.info(`Updating existing inventory with id ${this.props.inventory.id}`)
-          this.props.submitInventory(changedData, this.props.thing.id)
-        }
-      } else if (this.props.mode === DIALOG_NEW) {
-        log.info(`Creating new inventory named ${this.state.data.name}`)
-        this.props.submitInventory(this.state.data)
-      } else {
-        const errorMessage = `Unknown mode for InventoryEditDialog: ${String(this.props.mode)}`
-        log.error(errorMessage)
-        throw new Error(errorMessage)
-      }
-      // TODO: some sort of loading animation while waiting on the HTTP request
-    }
-  }
-
-  handleCancel = () => {
-    // Inventory form is very simple, do not bother with a confirmation
-    this.props.closeInventoryEditor()
-  }
-
-  render () {
-    const title = this.props.mode === DIALOG_EDIT
-      ? this.props.inventory.name : t('inventory.newTitle')
-    const form = <div>
-      <TextField name='name'
-        floatingLabelText={t('inventory.name')}
-        defaultValue={this.props.inventory.name}
-        errorText={this.state.errors.name}
-        onBlur={this.handleChange}
-      /><br />
-      {this.props.mode !== DIALOG_EDIT ? null : (<div>
-        Date added: {moment(this.inventory.date_created).calendar()}
-      </div>)}
-    </div>
-    return this.renderDialog(title, this.buttons, form, this.handleCancel)
-  }
-}
+  return <Dialog
+    open={true}
+    title={title}
+    actions={buttons}
+    onRequestClose={closeInventoryEditor}
+  >
+    <TextField name='name'
+      floatingLabelText={t('inventory.name')}
+      defaultValue={inventory.name}
+      errorText={errors.name}
+      onChange={handleChange}
+    />
+    {mode !== DIALOG_EDIT ? null : (<div><br />
+      Date added: {moment(inventory.date_created).calendar()}
+    </div>)}
+  </Dialog>
+}))
--- app/components/ThingEditDialog.js
+++ app/components/ThingEditDialog.js
@@ -1,6 +1,15 @@
+// React component to display a dialog for editing a thing.
+
+// Disable proptype linting, ESLint gets confused by
+// react-redux and Formik injected props on classes
+/* eslint-disable react/prop-types */
+
 import React from 'react'
 import { connect } from 'react-redux'
+import { Formik } from 'formik'
+import yup from 'yup'
 import log from 'loglevel'
+import Dialog from 'material-ui/Dialog'
 import FlatButton from 'material-ui/FlatButton'
 import RaisedButton from 'material-ui/RaisedButton'
 import TextField from 'material-ui/TextField'
@@ -8,17 +17,16 @@ import moment from 'moment'
 
 import { closeThingEditor, submitThing, removeThing } from '../actions'
 import t from '../i18n'
-import { isString, isEmpty } from '../util'
-import FormDialogBase from './FormDialogBase'
 import ConfirmDialog from './ConfirmDialog'
 
 const DIALOG_NEW = Symbol.for('ui.DIALOG_NEW')
 const DIALOG_EDIT = Symbol.for('ui.DIALOG_EDIT')
 const MULTILINE_ROWS = 3
 
-@connect(
+const reduxWrapper = connect(
   function mapStateToProps (state) {
     return {
+      // TODO: Throw error if current inventory does not exist
       currentInventoryId: state.database.inventories.length > 0
         ? state.database.inventories[state.ui.currentInventory].id
         : null
@@ -26,16 +34,89 @@ const MULTILINE_ROWS = 3
   },
   {submitThing, removeThing, closeThingEditor}
 )
-export default class ThingEditDialog extends FormDialogBase {
-  static fields = ['name', 'details', 'location']
-
+const formikWrapper = Formik({
+  mapPropsToValues: ({thing}) => ({
+    name: thing.name, details: thing.details, location: thing.location}
+  ),
+  validationSchema: yup.object().shape({
+    name: yup.string().required(t('thing.nameErrorMissing')),
+    details: yup.string().nullable(),
+    location: yup.string().nullable()
+  }),
+  handleSubmit: (values, {props}) => {
+    if (props.mode === DIALOG_EDIT) {
+      // TODO: Check for changes and submit only modified fields
+      log.info(`Updating existing thing with id ${props.thing.id}`)
+      props.submitThing(values, null, props.thing.id)
+    } else if (props.mode === DIALOG_NEW) {
+      log.info(`Creating new thing named ${values.name}`)
+      props.submitThing(values, props.currentInventoryId)
+    } else {
+      const errorMessage = `Unknown mode for ThingEditDialog: ${String(props.mode)}`
+      log.error(errorMessage)
+      throw new Error(errorMessage)
+    }
+    // TODO: some sort of loading animation while waiting on the HTTP request
+  }
+})
+@reduxWrapper
+@formikWrapper
+export default class ThingEditDialog extends React.Component {
   constructor (props) {
-    super(props, props.thing)
-    const thing = props.thing
-    this.title = this.props.mode === DIALOG_EDIT ? thing.name : t('thing.newTitle')
-    this.buttons = <div>
+    super(props)
+    // TODO: The confirm dialog is the only reason to use a stateful class
+    // component rather than a stateless funcional component. Consider using
+    // a higher-level generic component (possibly not specific to dialogs)
+    // that passes handleConfirm() as a prop and converting this one to a
+    // stateless component.
+    this.state = {confirm: {
+      open: false,
+      title: 'NO TITLE',
+      text: 'Confirm placeholder',
+      onYes: () => null,
+      onNo: () => null
+    }}
+  }
+
+  handleDelete = () => {
+    // Do not confirm, if a mistake is made user can retrieve it from the trash
+    this.props.removeThing(this.props.thing.id)
+    this.props.closeThingEditor()
+  }
+
+  handleCancel = () => {
+    // Show confirmation dialog if form data has changed
+    // TODO: Figure out why dirty isn't working
+    if (this.props.dirty) {
+      this.setState({confirm: {
+        open: true,
+        title: t('thing.confirmCancelTitle'),
+        text: t('thing.confirmCancelText'),
+        handleYes: this.props.closeThingEditor,
+        handleNo: () => this.setState({confirm: {open: false}})
+      }})
+    } else {
+      this.props.closeThingEditor()
+    }
+  }
+
+  render () {
+    log.info(this.props.values)
+    log.info(this.props.errors)
+    const values = this.props.values
+    const thing = this.props.thing
+    const title = this.props.mode === DIALOG_EDIT ? thing.name : t('thing.newTitle')
+    const confirmDialog = this.state.confirm.open
+      ? <ConfirmDialog
+        open={this.state.confirm.open}
+        title={this.state.confirm.title}
+        text={this.state.confirm.text}
+        onYes={() => { this.state.confirm.handleYes() }}
+        onNo={() => { this.state.confirm.handleNo() }} />
+      : null
+    const buttons = <div>
       { /* Hide delete button on new things */
-        props.mode !== DIALOG_NEW
+        this.props.mode !== DIALOG_NEW
           ? <FlatButton
             style={{float: 'left'}}
             label={t('thing.delete')}
@@ -50,108 +131,33 @@ export default class ThingEditDialog extends FormDialogBase {
       <RaisedButton
         primary={true}
         label={t('common.save')}
-        onClick={this.handleDone}
+        onClick={this.props.handleSubmit}
       />
     </div>
-    // Add confirm dialog info to state built in super()
-    this.state.confirm = {
-      open: false, title: 'NO TITLE', text: 'Confirm placeholder'
-    }
-  }
 
-  validateForm = () => {
-    const data = this.state.data
-    const errors = {}
-
-    if (isString(data.name)) {
-      if (data.name.length === 0) {
-        errors.name = t('thing.nameErrorMissing')
-      }
-    } else {
-      log.error(`Name should be a string, found ${typeof data.name} instead`)
-    }
-    if (data.location !== null && !isString(data.location)) {
-      log.error(`Location should be a string, found ${typeof data.location} instead`)
-    }
-    if (data.details !== null && !isString(data.details)) {
-      log.error(`Details should be a string, found ${typeof data.details} instead`)
-    }
-
-    this.setState({errors})
-    return isEmpty(errors)
-  }
-
-  handleDone = () => {
-    if (this.validateForm()) {
-      if (this.props.mode === DIALOG_EDIT) {
-        const changedData = this.getChangedData()
-        if (!isEmpty(changedData)) {
-          log.info(`Updating existing thing with id ${this.props.thing.id}`)
-          this.props.submitThing(changedData, null, this.props.thing.id)
-        }
-      } else if (this.props.mode === DIALOG_NEW) {
-        const newData = this.state.data
-        log.info(`Creating new thing named ${newData.name}`)
-        this.props.submitThing(newData, this.props.currentInventoryId)
-      } else {
-        const errorMessage = `Unknown mode for ThingEditDialog: ${String(this.props.mode)}`
-        log.error(errorMessage)
-        throw new Error(errorMessage)
-      }
-    }
-  }
-
-  handleDelete = () => {
-    // Do not confirm, if a mistake is made user can retrieve it from the trash
-    this.props.removeThing(this.props.thing.id)
-    this.props.closeThingEditor()
-  }
-
-  handleCancel = () => {
-    // Show confirmation dialog if form data has changed
-    if (!isEmpty(this.getChangedData())) {
-      this.setState({confirm: {
-        open: true,
-        title: t('thing.confirmCancelTitle'),
-        text: t('thing.confirmCancelText'),
-        handleYes: this.props.closeThingEditor,
-        handleNo: () => this.setState({confirm: {open: false}})
-      }})
-    } else {
-      this.props.closeThingEditor()
-    }
-  }
-
-  render () {
-    const thing = this.props.thing
-    const title = this.props.mode === DIALOG_EDIT ? thing.name : t('thing.newTitle')
-    const confirmDialog = this.state.confirm.open
-      ? <ConfirmDialog
-        open={this.state.confirm.open}
-        title={this.state.confirm.title}
-        text={this.state.confirm.text}
-        onYes={() => { this.state.confirm.handleYes() }}
-        onNo={() => { this.state.confirm.handleNo() }} />
-      : null
-    const form = <div>
+    return <Dialog
+      open={true}
+      title={title}
+      actions={buttons}
+      onRequestClose={this.handleCancel}
+    >
       <TextField name='name'
         floatingLabelText={t('thing.name')}
-        defaultValue={thing.name}
-        errorText={this.state.errors.name}
-        onBlur={this.handleChange} /><br />
+        defaultValue={values.name}
+        errorText={this.props.errors.name}
+        onChange={this.props.handleChange} /><br />
       <TextField name='details'
         floatingLabelText={t('thing.details')}
+        defaultValue={values.details}
         multiLine={true}
         rows={MULTILINE_ROWS} rowsMax={MULTILINE_ROWS}
         fullWidth={true}
-        defaultValue={thing.details}
-        onBlur={this.handleChange} /><br />
-      {this.props.mode !== DIALOG_EDIT ? null : (<div>
+        onChange={this.props.handleChange} />
+      {this.props.mode !== DIALOG_EDIT ? null : (<div><br />
         {t('thing.dateAdded')}: {moment(thing.date_created).calendar()}<br />
         {t('thing.dateModified')}: {moment(thing.date_modified).calendar()}
       </div>)}
       {confirmDialog}
-    </div>
-    return this.renderDialog(title, this.buttons, form, this.handleCancel)
+    </Dialog>
   }
 }
