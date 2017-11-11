22c4e3707c1c43d122802df23d8de4bca5d9812e
BitLooter <pwnzerdragoon+gh@gmail.com>
2017-11-10 22:06:58 -0800

    Admin app updated with new startup code

--- app/admin/index.js
+++ app/admin/index.js
@@ -5,13 +5,14 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import injectTapEventPlugin from 'react-tap-event-plugin'
 
 import startup, { renderStartupErrors } from '../common/startup'
+import { restoreUser } from '../common/actions/auth'
 import reducer from './reducers'
 import AdminApp from './components/AdminApp'
 import { refreshServerInfo, refreshStats } from './actions'
 
 renderStartupErrors(async () => {
   injectTapEventPlugin() // Needed for Material-UI
-  const store = await startup(
+  const {store, rerender} = await startup(
     // MuiThemeProvider: Needed for Material-UI
     <MuiThemeProvider>
       <AdminApp />
@@ -22,7 +23,7 @@ renderStartupErrors(async () => {
 
   // TODO: Do not load user if no api token set
   // TODO: Do not restore user until user sucessfully loads
-  // store.dispatch(restoreUser())
+  store.dispatch(restoreUser())
   // store.dispatch(loadUser())
   // TODO: Load stats on status panel select
   store.dispatch(refreshStats())
@@ -30,6 +31,7 @@ renderStartupErrors(async () => {
 
   // Set up HMR for dev server
   if (module.hot) {
+    module.hot.accept('./components/AdminApp', rerender)
     module.hot.accept('./reducers', () => store.replaceReducer(reducer))
     module.hot.accept('./actions')
   }
